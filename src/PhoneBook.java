f9729ca21920abdfd0630a97a19d1fd202e185e2
Pavel Yanovsky <humgate@gmail.com>
2020-09-28 02:21:35 +0300

    Entering cycle added in the method main for reading 3 values: name, phonenumber and repeat. Implemented methods: add, list. Calls for methods main and list added in method main

--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -3,37 +3,55 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PhoneBook {
-    static String [][] phoneBook= new String [10][10];
+    static String[][] phoneBook = new String[10][10];
+    static Scanner scanner = new Scanner(System.in);
     static int recCount = 0;
 
     public static void main(String[] args) {
-
         //Добавить считывание ввода пользователя в цикле
-        Scanner scanner = new Scanner(System.in);
         String inputName = "";
         String inputNumber = "";
+        String readAnotherRecord = "y";
         boolean isCorrectInput;
 
-        /* Запрашиваем ввести ФИО */
-        isCorrectInput = false;
-        while (!isCorrectInput) {
-            System.out.println("Введите ФИО. Три слова разделенные пробелами:");
-            inputName = scanner.nextLine(); //Считывает строку из System.in
-            isCorrectInput = checkName(inputName);
-            if (!isCorrectInput) System.out.println("ФИО введено некорректно. Введите корректные ФИО!");
-        }
+        do {
+            /* Запрашиваем ввести ФИО */
+            isCorrectInput = false;
+            while (!isCorrectInput) {
+                System.out.println("Введите ФИО. Три слова разделенные пробелами:");
+                inputName = scanner.nextLine(); //Считывает строку из System.in
+                isCorrectInput = checkName(inputName);
+                if (!isCorrectInput) System.out.println("ФИО введено некорректно. Введите корректные ФИО!");
+            }
 
-        /* Запрашиваем ввести номер */
-        isCorrectInput = false;
-        while (!isCorrectInput) {
-            System.out.println("Введите номер, 11 цифр. ФорматК 8(XXX)-XXX-XX-XX. ");
-            inputNumber = scanner.nextLine(); //Считывает строку из System.in
-            isCorrectInput = checkPhoneNumber(inputNumber);
-            if (!isCorrectInput) System.out.println("Номер введен некорректно. Повторите ввод!");
-        }
+            /* Запрашиваем ввести номер */
+            isCorrectInput = false;
+            while (!isCorrectInput) {
+                System.out.println("Введите номер, 11 цифр. ФорматК 8(XXX)-XXX-XX-XX. ");
+                inputNumber = scanner.nextLine(); //Считывает строку из System.in
+                isCorrectInput = checkPhoneNumber(inputNumber);
+                if (!isCorrectInput) System.out.println("Номер введен некорректно. Повторите ввод!");
+            }
 
-        add(phoneBook, inputName, inputNumber);
+            /* Добавляем знаение в справочник */
+            add(phoneBook, inputName,  formatPhoneNumber(inputNumber));
 
+            /* Печатем весь справочник */
+            list(phoneBook, recCount);
+
+            System.out.println("Добавить еще одно значение в справочник y/n:");
+
+            /* Запрашиваем нужно ли добавить еще записи в справочник */
+            isCorrectInput = false;
+            while (!isCorrectInput) {
+                System.out.println("Введите y/n: ");
+                readAnotherRecord = scanner.nextLine(); //Считывает строку из System.in
+                if (readAnotherRecord.equals("y") || readAnotherRecord.equals("n")) {
+                    isCorrectInput = true;
+                }
+                if (!isCorrectInput) System.out.println("Введите y/n: ");
+            }
+        } while (readAnotherRecord.equals("y"));
     }
 
     public static boolean checkPhoneNumber(String phoneNumber) {
@@ -63,20 +81,20 @@ public class PhoneBook {
              одна (или не одной) комбинация дефиса и одной заглавной буквы, затем любое кол-во (в т.ч. ноль)
              прописных букв, заканчиваться должно обязательно одним пробелом
              */
-            "[A-ZА-Я][a-zа-я]*(-[A-ZА-Я])?[a-zа-я]*\\s" +
+                "[A-ZА-Я][a-zа-я]*(-[A-ZА-Я])?[a-zа-я]*\\s" +
 
              /* Имя.
              Обязательно одна и только одна заглаваня буква, затем любое кол-во (в т.ч. ноль) прописных букв, затем
              одна (или не одной) комбинация дефиса и одной заглавной буквы, затем любое кол-во (в т.ч. ноль)
              прописных букв, заканчиваться должно обязательно одним пробелом
              */
-            "[A-ZА-Я][a-zа-я]*(-[A-ZА-Я])?[a-zа-я]*\\s" +
+                        "[A-ZА-Я][a-zа-я]*(-[A-ZА-Я])?[a-zа-я]*\\s" +
              /* Отчество. Для разнообразия сделаем немного по другоме чем для фамилии и имени.
              Обязательно одна и только одна заглаваня буква, затем любое кол-во (в т.ч. ноль) прописных букв, затем
              одна (или не одной) комбинация дефиса и любой буквы, затем любое кол-во (в т.ч. ноль)
              прописных букв
              */
-            "[A-ZА-Я][a-zа-я]*(-[A-ZА-Яa-zа-я])?[a-zа-я]*"
+                        "[A-ZА-Я][a-zа-я]*(-[A-ZА-Яa-zа-я])?[a-zа-я]*"
         );
         Matcher matcher = pattern.matcher(name);
         result = matcher.matches();
@@ -88,31 +106,37 @@ public class PhoneBook {
         result= words.length == 3;
         */
         return result;
-        }
+    }
 
     public static String formatName(String name) {
         return "";
     }
 
     public static String formatPhoneNumber(String number) {
-        return "";
+        String result = number.replaceAll ("[^\\d]", "");
+        result = "+7 " + result.substring(1,4) + " " + result.substring(4,7) +
+                " " + result.substring(7,9) + " " + result.substring(9,11);
+        return result;
     }
 
     public static void add(String[][] book, String name, String number) {
         //add logic
         if (recCount < 10) {
-            recCount++;
             book[recCount][0] = name;
             book[recCount][1] = number;
+            recCount++;
             System.out.println("Запись успешно добавлена");
         } else {
             System.out.println("Превышено максмальное количество записей справочника. Новая запись не добавлена!");
         }
-
     }
 
-    public static void list(String[][] book) {
+    public static void list(String[][] book, int booklength) {
         //print phone book
+        System.out.println("Список значений справочника:");
+        for (int i = 0; i < booklength; i++) {
+            System.out.println(i + ". " + book[i][0] + ": " + book[i][1]);
+        }
     }
-
 }
+
